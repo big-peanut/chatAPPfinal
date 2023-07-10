863c7ff9ae23927504140fdbbe495290aecf7921
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-07-10 11:43:33 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,3 +29,30 @@ exports.signup = async (req, res, next) => {
         res.status(500).json({ error: "Failed to sign up" });
     }
 };
+
+function generateAccessToken(id, name) {
+    return jwt.sign({ id: id, name: name }, secretkey);
+}
+
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        const existingUser = await Users.findOne({ where: { email: email } });
+
+        if (!existingUser) {
+            return res.status(404).json({ error: "Email not registered. Please sign up" });
+        }
+
+        const result = await bcrypt.compare(password, existingUser.password);
+
+        if (result) {
+            const token = generateAccessToken(existingUser.id, existingUser.name);
+            return res.status(200).json({ message: "User login successful", token: token });
+        } else {
+            return res.status(400).json({ error: "Incorrect password" });
+        }
+    } catch (err) {
+        res.status(500).json({ error: "Failed to login" });
+    }
+};
